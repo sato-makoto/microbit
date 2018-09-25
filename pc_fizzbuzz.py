c452c6a1342cd1e455b16e214b20be125c380005
Sato Makoto <makoto@kuzuore.com>
2018-09-25 23:37:17 +0900

    changed to fizzbuzz

--- pc_fizzbuzz.py
+++ pc_fizzbuzz.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 maxnum = 2**25
 
-for x in range(maxnum):
+for x in range(1, maxnum + 1):
+    if x % 15 == 0:
+        print('fizzbuzz')
+    elif x % 5 == 0:
+        print('buzz')
+    elif x % 3 == 0:
+        print('fizz')
+    else:
         numstr = str(bin(x))[2:].replace('1', '9')
         emp_str = "% 25s" % numstr
         zero_str = emp_str.replace(' ', '0')
