040122fb28842bdc3ea9ba224357cceabfe86215
Sato Makoto <makoto@kuzuore.com>
2018-10-06 16:41:32 +0900

    round circle

--- run_around.py
+++ run_around.py
@@ -1,15 +1,12 @@
 from microbit import *
 x, y, i = 0, 0, 0
-w = 100
-round = [[x, y] for x in range(4)]
-round += [[4, y] for y in range(4)]
-round += [[x, 4] for x in range(4, 0, -1)]
-round += [[0, y] for y in range(4, 0, -1)]
-round.reverse()
+w = 50
+round = [[x, y] for x in range(1,4)]
+round += [[4, y] for y in range(1,4)]
+round += [[x, 4] for x in range(3, 0, -1)]
+round += [[0, y] for y in range(3, 0, -1)]
 
-display.set_pixel(0,0,9)
-sleep(w)
-display.clear()
+round.reverse()
 
 while True:
     display.set_pixel(round[i][0], round[i][1], 9)
