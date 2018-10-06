7d3749d61316a9e261706e9a63d40f368f684e61
Sato Makoto <makoto@kuzuore.com>
2018-10-06 16:51:23 +0900

    add tail

--- run_around.py
+++ run_around.py
@@ -1,6 +1,6 @@
 from microbit import *
 x, y, i = 0, 0, 0
-w = 50
+w = 100
 round = [[x, y] for x in range(1,4)]
 round += [[4, y] for y in range(1,4)]
 round += [[x, 4] for x in range(3, 0, -1)]
@@ -10,6 +10,9 @@ round.reverse()
 
 while True:
     display.set_pixel(round[i][0], round[i][1], 9)
+    display.set_pixel(round[i-1][0], round[i-1][1], 7)
+    display.set_pixel(round[i-2][0], round[i-2][1], 5)
+    display.set_pixel(round[i-3][0], round[i-3][1], 4)
     if i == len(round)-1:
         i = 0
     else:
