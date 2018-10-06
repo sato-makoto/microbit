890b99bfe1d6c320d2f19544c093b09f0d682bbb
Sato Makoto <makoto@kuzuore.com>
2018-10-06 14:08:29 +0900

    changed the way of running

--- run_around.py
+++ run_around.py
@@ -1,20 +1,23 @@
 from microbit import *
-s = 200
-
+x, y, i, w = 0, 0, 0, 0
+w_ori = 800
+round = [[x, y] for x in range(5)]
+round += [[4, y] for y in range(5)]
+round += [[x, 4] for x in range(4, -1, -1)]
+round += [[0, y] for y in range(4, -1, -1)]
 while True:
-    for x in range(0,5):
-        display.set_pixel(x,0,9)
-        sleep(s)
-        display.set_pixel(x,0,0)
-    for y in range(0,5):
-        display.set_pixel(x,y,9)
-        sleep(s)
-        display.set_pixel(x,y,0)
-    for x in range(4,-1,-1):
-        display.set_pixel(x,y,9)
-        sleep(s)
-        display.set_pixel(x,y,0)
-    for y in range(4,-1,-1):
-        display.set_pixel(x,y,9)
-        sleep(s)
-        display.set_pixel(x,y,0)
+    if round[i] == [0, 0] \
+            or round[i] == [0, 4] \
+            or round[i] == [4, 0] \
+            or round[i] == [4, 4]:
+        w = w_ori/2
+    else:
+        w = w_ori
+    display.set_pixel(round[i][0], round[i][1], 9)
+    if i == 19:
+        i = 0
+    else:
+        i += 1
+    sleep(w)
+    display.clear()       
+        
