87127d3fefbe65a5af9166af0587124465d86cd2
Sato Makoto <makoto@kuzuore.com>
2020-04-26 03:27:53 +0900

    speed up

--- banana.py
+++ banana.py
@@ -1,20 +1,21 @@
 # ここにコードを書いてね :-)
 from microbit import *
-
+# バナナ + バナナ = シナモン
+# a: バ b:ナ c:シ d:モ e:ン
 
 def calc():
+    c = 1
     for a in range(1, 10):
         for b in range(10):
-            for c in range(1, 10):
-                for d in range(10):
-                    for e in range(10):
-                        check = {a, b, c, d, e}
-                        banana = a * 100 + b * 10 + b
-                        answer = c * 1000 + b * 100 + d * 10 + e
-                        if banana * 2 == answer and len(check) == 5:
-                            b2 = str(banana) + ' + ' + str(banana)
-                            all = b2 + ' = ' + str(answer)
-                            display.scroll(all)
+            for d in range(10):
+                for e in range(10):
+                    check = {a, b, c, d, e}
+                    banana = a * 100 + b * 10 + b
+                    answer = 1000 + b * 100 + d * 10 + e
+                    if banana * 2 == answer and len(check) == 5:
+                        b2 = str(banana) + ' + ' + str(banana)
+                        all = b2 + ' = ' + str(answer)
+                        display.scroll(all)
 
 display.show(Image.CHESSBOARD)
 while True:
@@ -24,3 +25,4 @@ while True:
         display.show(Image.HAPPY)
         sleep(2000)
         display.clear()
+    display.show(Image.CHESSBOARD)
